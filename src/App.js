d8713b726fd0a8aeea3c646bfe00b161fcc106a4
Juniore <babanarowerze5@wp.pl>
2019-04-23 15:23:23 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,3 +1,82 @@
+// import React, { Component } from 'react';
+// import './App.css';
+
+// class App extends Component {
+
+//   state = {
+//     board: Array(9).fill(null),
+//     player: "O"
+//   }
+
+//   board2 = Array(12).fill({ id: 1 })
+
+
+// checkWinner = () => {
+
+//   let winLines = [
+//     ["0", "1", "2"],
+//     ["3", "4", "5"],
+//     ["6", "7", "8"],
+//     ["0", "3", "6"],
+//     ["1", "4", "7"],
+//     ["2", "5", "8"],
+//     ["0", "4", "8"],
+//     ["2", "4", "6"],
+
+//   ]
+//   for (let index = 0; index < winLines.length; index++) {
+
+//     const [a, b, c] = winLines[index]
+//     console.log(this.state.board[a]);
+
+//     if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] ===
+//       this.state.board[c]) {
+
+//       alert('you won')
+//     }
+//   }
+
+// }
+//   handelClick = (index) => {
+
+//     let newBoard = this.state.board
+
+
+//     if (this.state.board[index] === null) {
+
+//       newBoard[index] = this.state.player
+//       let newPlayer = this.state.player === "X" ? "0" : "X"
+
+//       this.setState({
+//         board: newBoard,
+//         player: newPlayer
+//       })
+//       this.checkWinner()
+//     }
+//   }
+
+
+//   render() {
+
+//     const Box = this.state.board.map((box, index) => <div onClick={() => this.handelClick(index)} key={index} className="box">{box}</div>)
+//     return (
+//       <div className="container">
+//         <h1>Tic-tac-toe-app</h1>
+//         <div className="board">
+//           {Box}
+//         </div>
+//       </div>
+
+//     )
+
+//   }
+// }
+
+
+// export default App;
+
+
+
 import React, { Component } from 'react';
 import './App.css';
 
@@ -5,11 +84,9 @@ class App extends Component {
 
   state = {
     board: Array(9).fill(null),
-    player: "O"
+    player: "X",
+    winner: null
   }
-
-  board2 = Array(12).fill({ id: 1 })
-
 
   checkWinner = () => {
 
@@ -24,26 +101,26 @@ class App extends Component {
       ["2", "4", "6"],
 
     ]
+
     for (let index = 0; index < winLines.length; index++) {
 
       const [a, b, c] = winLines[index]
-      console.log(this.state.board[a]);
-
-      if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] ===
-        this.state.board[c]) {
-
-        alert('you won')
+      console.log(this.state.board[a])
+      if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c]) {
+        alert("you won")
+        this.setState({
+          winner: this.state.player
+        })
       }
+
     }
 
   }
+
+
   handelClick = (index) => {
-
     let newBoard = this.state.board
-
-
-    if (this.state.board[index] === null) {
-
+    if (this.state.board[index] === null && !this.state.winner) {
       newBoard[index] = this.state.player
       let newPlayer = this.state.player === "X" ? "0" : "X"
 
@@ -55,13 +132,18 @@ class App extends Component {
     }
   }
 
-
   render() {
+    console.log(null)
 
-    const Box = this.state.board.map((box, index) => <div onClick={() => this.handelClick(index)} key={index} className="box">{box}</div>)
+    const Box = this.state.board.map((box, index) => <div
+      onClick={() => this.handelClick(index)}
+      key={index}
+      className="box">
+      {box}
+    </div>)
     return (
       <div className="container">
-        <h1>Tic-tac-toe-app</h1>
+        <h1>Tic Tac Toe Game</h1>
         <div className="board">
           {Box}
         </div>
